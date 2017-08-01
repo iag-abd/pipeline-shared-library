1d3d84d8a01adaf19accfa6e89ab65e1f9675dd3
daragh martin <Daragh.Martin@iag.com.au>
2017-08-02 07:24:13 +1000

    debug slack 1

--- vars/slackOut.groovy
+++ vars/slackOut.groovy
@@ -3,21 +3,21 @@
 import groovy.json.JsonOutput
 
 def call(config = [:]) {
-  // if (!(config.attachments || config.text)) throw new Exception("need text or attacment or both ${config}")
+  if (!(config.attachments || config.text)) throw new Exception("need text or attacment or both ${config}")
+
+  //config.text = config.text ?: "hello from ${env.BUILD_ID}"
+  config.channel = config.channel ?: env.SLACK_CHANNEL ?: "jenkins"
+  config.username = config.username ?: env.SLACK_USERNAME ?: "jenkinsbot"
+  config.iconEmoji = config.iconEmoji ?: env.SLACK_ICON_EMOJI ?: ":robot_face:"
+  //what is the credential called?
+  config.slackUriCredentialsId = config.slackUriCredentialsId ?: 'slack_uri'
+  // config.proxyHost = config.proxyHost ?: env.SLACK_PROXY ?: null
+  // config.proxyPort = config.proxyPort ?: env.SLACK_PROXY_PORT ?: null
   //
-  // //config.text = config.text ?: "hello from ${env.BUILD_ID}"
-  // config.channel = config.channel ?: env.SLACK_CHANNEL ?: "jenkins"
-  // config.username = config.username ?: env.SLACK_USERNAME ?: "jenkinsbot"
-  // config.iconEmoji = config.iconEmoji ?: env.SLACK_ICON_EMOJI ?: ":robot_face:"
-  // //what is the credential called?
-  // config.slackUriCredentialsId = config.slackUriCredentialsId ?: 'slack_uri'
-  // // config.proxyHost = config.proxyHost ?: env.SLACK_PROXY ?: null
-  // // config.proxyPort = config.proxyPort ?: env.SLACK_PROXY_PORT ?: null
-  // //
-  // // if (config.proxyHost) {
-  // //   if (!(config.proxyPort)) throw new Exception("Proxy host but no proxy port")
-  // // }
-  //
+  // if (config.proxyHost) {
+  //   if (!(config.proxyPort)) throw new Exception("Proxy host but no proxy port")
+  // }
+
   // def payloadJson
   //
   // message = [
