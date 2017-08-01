7fb91c6ddb28d88c2ce63f356bd7867f6f58f0cd
daragh martin <Daragh.Martin@iag.com.au>
2017-08-02 06:30:35 +1000

    debug ecr 0

--- vars/ecrDockerLogin.groovy
+++ vars/ecrDockerLogin.groovy
@@ -1,8 +1,14 @@
 #!/usr/bin/groovy
 
-call(String region){
-  echo 'login to docker'
-  sh "eval `aws ecr get-login --region ${region}`"
-  echo 'test that login'
-  sh "aws --region ${region} ecr describe-repositories"
+// call(String region){
+//   // echo 'login to docker'
+//   // sh "eval `aws ecr get-login --region ${region}`"
+//   // echo 'test that login'
+//   // sh "aws --region ${region} ecr describe-repositories"
+// }
+
+def call(config) {
+  // evaluate the body block, and collect configuration into the object
+  config = config ?: [:]
+  echo config.region
 }
