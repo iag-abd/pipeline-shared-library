c474f94d659c73b0992b9295f694986feb4cbaef
daragh martin <Daragh.Martin@iag.com.au>
2017-08-02 06:46:38 +1000

    ecr working

--- vars/ecrCreateRepo.groovy
+++ vars/ecrCreateRepo.groovy
@@ -2,7 +2,6 @@
 
 def call(config) {
   // evaluate the body block, and collect configuration into the object
-  config = config ?: [:]
   echo 'create repo if it does not exist'
   sh "aws --region ${config.region} ecr create-repository --repository-name ${config.repo} || echo 'repo exists already'"
 }
--- vars/ecrDockerLogin.groovy
+++ vars/ecrDockerLogin.groovy
@@ -1,8 +1,6 @@
 #!/usr/bin/groovy
 
 def call(config) {
-  // evaluate the body block, and collect configuration into the object
-  config = config ?: [:]
   echo 'login to docker in ' + config.region
   sh "eval `aws ecr get-login --region ${config.region}`"
   echo 'test ecr login'
