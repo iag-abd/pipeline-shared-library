92807f1a2056f56e734b8c445d3f8ee781b660e7
daragh martin <Daragh.Martin@iag.com.au>
2017-08-02 05:29:23 +1000

    debug serialisable 1

--- vars/getAWSDetails.groovy
+++ vars/getAWSDetails.groovy
@@ -3,8 +3,8 @@
 def call() {
   az = sh(returnStdout: true, script: 'curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone').trim()
   echo az
-  //region = az.replaceFirst(/[a-z]$/, "")
-  //echo "region: ${REGION}"
+  region = az.replaceFirst(/[a-z]$/, "")
+  echo "region: ${REGION}"
   //account = sh(returnStdout: true, script: 'curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep -oP \'(?<="accountId" : ")[^"]*(?=")\'').trim()
   //ecr = "${account}.dkr.ecr.${region}.amazonaws.com"
 
