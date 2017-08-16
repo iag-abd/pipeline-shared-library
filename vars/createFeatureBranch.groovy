d95faeb6ef36549af635629c516bbe798d353043
iag abd builder <iagabdbuilder@jenkins.iag.com>
2017-08-16 16:35:01 +1000

    update maps default for feature branch create

--- vars/createFeatureBranch.groovy
+++ vars/createFeatureBranch.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 
 def call(config = [:]) {
-  version = config.version ?: "jenkins-${BUILD_ID}"
+  config.version = config.version ?: "jenkins-build-${BUILD_ID}"
   config.branch = config.branch ?: 'master'
   sh "git checkout -b feature/${version} ${config.branch}"
 }
