51a0b6b5b2d8efdbe80468897f0232ae458fd65f
iag abd builder <iagabdbuilder@jenkins.iag.com>
2017-08-16 16:40:19 +1000

    fix version

--- vars/createFeatureBranchForFlow.groovy
+++ vars/createFeatureBranchForFlow.groovy
@@ -2,5 +2,5 @@
 
 def call(config = [:]) {
   config.version = config.version ?: "jenkins-build-${BUILD_ID}"
-  sh "git checkout -b feature/${version} develop"
+  sh "git checkout -b feature/${config.version} develop"
 }
