9b11eaae736c5b92c3a50cf63efbc8b364494c05
daraghmartin <daragh@daragh.net>
2018-06-05 10:25:16 +1000

    update docker login

--- vars/ecrDockerLogin.groovy
+++ vars/ecrDockerLogin.groovy
@@ -2,7 +2,7 @@
 
 def call(config) {
   echo 'login to docker in ' + config.region
-  sh "eval `aws ecr get-login --region ${config.region}`"
-  echo 'test ecr login'
-  sh "aws --region ${config.region} ecr describe-repositories"
+  sh "eval `aws ecr get-login --no-include-email --region ${config.region}`"
+  //echo 'test ecr login'
+  //sh "aws --region ${config.region} ecr describe-repositories"
 }
