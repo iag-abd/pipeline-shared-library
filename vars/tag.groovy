2fe22813dc8fceef9521bfed92e82a3424d4ddf1
iag abd builder <iagabdbuilder@jenkins.iag.com>
2018-07-19 07:17:00 +1000

    add commit to tags

--- vars/tag.groovy
+++ vars/tag.groovy
@@ -3,9 +3,11 @@
 def call(config = [:]) {
   config.branch = config.branch ?: 'master'
   config.version = config.version ?: "jenkins-build-${BUILD_ID}"
+  config.commit = config.commit ?: config.branch
+
   sshagent (credentials: [config.credentialsId]) {
     sh """
-      git tag -a ${config.version} -m \"Build on ${JOB_DISPLAY_URL} at `date -d today +'%Y%m%d%H%M'`\" ${config.branch}
+      git tag -a ${config.version} -m \"Build on ${JOB_DISPLAY_URL} at `date -d today +'%Y%m%d%H%M'`\" ${config.commit}
       git push --tags
     """
   }
