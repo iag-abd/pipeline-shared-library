a2b16332700bb8140da177689ade28f52985424b
iag abd builder <iagabdbuilder@jenkins.iag.com>
2018-07-18 13:23:21 +1000

    tidy up ssmversion

--- vars/ssmVersion.groovy
+++ vars/ssmVersion.groovy
@@ -9,23 +9,20 @@ def call(config = [:]) {
     versionParam = config.versionParam
     region = config.region
 
-    sh "aws ssm get-parameter --name $versionParam --region ap-southeast-2 || aws ssm put-parameter --name $versionParam --type String --value 0.0.0 --overwrite --region $region"
+    sh "aws ssm get-parameter --name $versionParam --region $region || aws ssm put-parameter --name $versionParam --type String --value 0.0.0 --overwrite --region $region"
 
     version = sh (
         script: "aws ssm get-parameter --name $versionParam --region $region| jq ' .[] | .Value' --raw-output",
         returnStdout: true
     ).trim()
           
-    echo version
-          
     def matcher = version =~ /(?<major>\d*).(?<minor>\d*).(?<revision>\d*)[.-]*(.*)/
-    v2 = matcher[0][1] + "." + matcher[0][2] + "." + (Integer.parseInt(matcher[0][3]) + 1)
-
-    version = v2
+    version = matcher[0][1] + "." + matcher[0][2] + "." + (Integer.parseInt(matcher[0][3]) + 1)
   }
 
   script {
     sh "aws ssm put-parameter --name $config.versionParam --type String --value $version --overwrite --region $config.region"
   }
+  
   return version
 }
